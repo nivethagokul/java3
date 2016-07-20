392d0d6dcabf4e589ac0fcbdbfc08a106dafc687
nivethagokul <umagovindan22@gmail.com>
2016-07-20 12:29:14 +0530

    tyjty
    gjkytk

---
+++ raj.java
@@ -0,0 +1,18 @@
+public class natno {
+
+	public static void main(String[] args) {
+		int x;
+		x=Integer.parseInt(args[0]);
+		
+		int i;
+		int y = 0;
+		for(i=0;i<=6;i++)
+		{
+			y=y+i;
+		}
+			System.out.println(y);
+		
+
+	}
+
+}
